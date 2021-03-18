1738d29289ed1b8cf1243c2f0e8ff9fcc63fca37
Vitche Research Team Developer <developer@vitche.com>
2021-03-18 19:47:21 +0200

    1. Launching the reverse SSH tunnel.

--- main.js
+++ main.js
@@ -21,59 +21,45 @@ const killTree = function (pid, signal, callback) {
 module.exports.from = function (localHost, localPort) {
   return {
     through: function (proxyType, proxyHost, proxyPort, proxyUser, proxyPassword) {
-      if ("ssh" === proxyType) {
-        // TCP proxying through "SSH"
-        return {
-          to: function (serverHost, serverPort) {
-            return {
-              start: function () {
-                const command = `sshpass -p '${proxyPassword}' ssh -R ${serverPort}:${serverHost}:${localPort} -N ${proxyUser}@${proxyHost} -p ${proxyPort}`;
-                console.log(command);
-                const tunnel = {
-                  process: undefined,
-                  close: () => {
-                  }
-                };
+      return {
+        to: function (serverHost, serverPort) {
+          return {
+            start: function () {
+              let key = localHost + ':' + localPort;
+              let tunnel = cache.get(key);
+              if (tunnel) {
                 return tunnel;
               }
-            };
-          }
-        };
-      } else {
-        // TCP proxying through "ncat"
-        return {
-          to: function (serverHost, serverPort) {
-            return {
-              start: function () {
-                let key = localHost + ':' + localPort;
-                let tunnel = cache.get(key);
-                if (tunnel) {
-                  return tunnel;
+              let command;
+              if ("ssh" === proxyType) {
+                // TCP proxying through "SSH"
+                command = `sshpass -p '${proxyPassword}' ssh -R ${serverPort}:${serverHost}:${localPort} -N ${proxyUser}@${proxyHost} -p ${proxyPort}`;
+              } else {
+                // TCP proxying through "ncat"
+                command = 'ncat -l ' + localHost + ' ' + localPort + ' --keep-open --sh-exec "ncat --proxy ' + proxyHost + ':' + proxyPort + ' --proxy-type ' + proxyType + ' ' + serverHost + ' ' + serverPort + '"';
+              }
+              console.log('Establishing tunnel: ' + command + "\n");
+              let childProcess = exec(command, (error, stdout, stderr) => {
+                if (stdout) {
+                  console.log('Tunnel output: ' + stdout);
                 }
-                let command = 'ncat -l ' + localHost + ' ' + localPort + ' --keep-open --sh-exec "ncat --proxy ' + proxyHost + ':' + proxyPort + ' --proxy-type ' + proxyType + ' ' + serverHost + ' ' + serverPort + '"';
-                console.log('Establishing tunnel: ' + command + "\n");
-                let childProcess = exec(command, (error, stdout, stderr) => {
-                  if (stdout) {
-                    console.log('Tunnel output: ' + stdout);
-                  }
-                  if (stderr) {
-                    console.log('Tunnel error: ' + stderr);
-                  }
-                });
-                tunnel = {
-                  process: childProcess,
-                  close: function (callback) {
-                    killTree(childProcess.pid, 'SIGKILL', function (error) {
-                      if (!error) {
-                        cache.del(key);
-                      }
-                      callback(error);
-                    });
-                  }
-                };
-                cache.put(key, tunnel);
-                return tunnel;
-              }
+                if (stderr) {
+                  console.log('Tunnel error: ' + stderr);
+                }
+              });
+              tunnel = {
+                process: childProcess,
+                close: function (callback) {
+                  killTree(childProcess.pid, 'SIGKILL', function (error) {
+                    if (!error) {
+                      cache.del(key);
+                    }
+                    callback(error);
+                  });
+                }
+              };
+              cache.put(key, tunnel);
+              return tunnel;
             }
           }
         }
